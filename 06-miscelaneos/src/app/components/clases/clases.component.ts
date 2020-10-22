099e7a299fb9d4842857a07b1a5a7cbd6f84c9be
txitxarra <ma.martinezlorca@gmail.com>
2020-10-22 21:46:39 +0200

    Sexta parte

---
+++ 06-miscelaneos/src/app/components/clases/clases.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html',
+  styles: [
+  ]
+})
+export class ClasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
